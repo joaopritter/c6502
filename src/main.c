c95813be7fac6ac1d4a111423f42573e47112448
joaopritter <pedro.ritter@pm.me>
2026-03-24 14:07:38 -0300

    doc: add sources and refs

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// Sources and Refs:
+// - https://www.nesdev.org/wiki/
+// - https://bugzmanov.github.io/nes_ebook/
+// - https://github.com/gianlucag/mos6502
+
 int main(void) {
   uint8_t program[3] = {0xA9, 0xC0, 0x00};
   CPU cpu;

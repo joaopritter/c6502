d28cc0f897cad1b48f6c07e9ca9ce100678c6c17
João Pedro Ritter <pedro.ritter@pm.me>
2026-03-25 21:00:10 +0000

    feat: flag handling methods

---
+++ include/cpu/flags.h
@@ -0,0 +1,41 @@
+#ifndef FLAGS_H
+#define FLAGS_H
+#include <stdint.h>
+
+#include "cpu/control.h"
+
+#define NEGATIVE_FLAG           0b10000000
+#define OVERFLOW_FLAG           0b01000000
+#define B_FLAG                  0b00010000
+#define DECIMAL_FLAG            0b00001000
+#define INTERRUPT_DISABLE_FLAG  0b00000100
+#define ZERO_FLAG               0b00000010
+#define CARRY_FLAG              0b00000001
+
+int check_flag(Control *c, int flag);
+
+void assign_flag(Control *c, int set, int flag);
+
+int check_negative_flag(Control *c);
+
+void assign_negative_flag(Control *c, uint8_t result);
+
+int check_overflow_flag(Control *c);
+
+void assign_overflow_flag(Control *c, uint8_t is_overflow);
+
+int check_interrupt_disable_flag(Control *c);
+
+void assign_interrupt_disable_flag(Control *c, uint8_t set);
+
+int check_zero_flag(Control *c);
+
+void assign_zero_flag(Control *c, uint8_t result);
+
+void assign_nz_flags(Control *c, uint8_t result);
+
+int check_carry_flag(Control *c);
+
+void assign_carry_flag(Control *c, uint8_t is_carry);
+
+#endif

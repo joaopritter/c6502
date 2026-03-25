bb914c2f00b9a08157bcac45e6c2c34267467263
joaopritter <pedro.ritter@pm.me>
2026-03-25 02:00:32 -0300

    tests: test_0xa9

--- tests/test_instr.c
+++ tests/test_instr.c
@@ -1,5 +1,6 @@
 #include "cpu/central.h"
 #include "unity.h"
+#include "unity_internals.h"
 
 void setUp(void) {
   // runs before each test
@@ -9,17 +10,26 @@ void tearDown(void) {
   // runs after each test
 }
 
-void test_tax(void) {
+void test_0xa9(void) {
+  CPU cpu = init_cpu();
+  uint8_t program[3] = {0xA9, 0x05, 0x00};
+  load(&cpu, program);
+  run(&cpu);
+  TEST_ASSERT_EQUAL(cpu.control.a, 0x05);
+}
+
+void test_0xaa(void) {
   CPU cpu = init_cpu();
   cpu.control.a = 10;
   uint8_t program[2] = {0xAA, 0x00};
   load(&cpu, program);
   run(&cpu);
-  TEST_ASSERT_EQUAL(10, cpu.control.x);
+  TEST_ASSERT_EQUAL(cpu.control.x, 10);
 }
 
 int main(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_tax);
+  RUN_TEST(test_0xa9);
+  RUN_TEST(test_0xaa);
   return UNITY_END();
 }

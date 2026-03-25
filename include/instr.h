1982bbb121a953ba41953e36f12d49bf59bf1cb2
joaopritter <pedro.ritter@pm.me>
2026-03-25 11:15:02 -0300

    doc: instructions comments

--- include/instr.h
+++ include/instr.h
@@ -4,11 +4,16 @@
 #include "cpu/control.h"
 #include <stdint.h>
 
+// Loads a memory value into the accumulator.
 void LDA(Control *c, uint16_t addr);
 
+// Copies the accumulator value to the X register.
 void TAX(Control *c, uint16_t addr);
 
+// Triggers an interrupt request (IRQ).
+// TODO: Proper IRQ handling.
 void BRK(Control *c, uint16_t addr);
 
+// Dummy instruction that does nothing.
 void ILL(Control *c, uint16_t addr);
 #endif

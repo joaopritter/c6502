309aa8526898e01aa8fe1dc31ce201db723d3785
joaopritter <pedro.ritter@pm.me>
2026-03-26 01:28:00 -0300

    clean: organize instr.h

--- include/instr.h
+++ include/instr.h
@@ -4,6 +4,10 @@
 #include "cpu/control.h"
 #include <stdint.h>
 
+// Triggers an interrupt request (IRQ).
+// TODO: Proper IRQ handling.
+void BRK(Control *c, uint16_t addr);
+
 // Loads a value into the accumulator.
 void LDA(Control *c, uint16_t addr);
 
@@ -12,9 +16,6 @@ void LDX(Control *c, uint16_t addr);
 
 // Loads a value into X.
 void LDY(Control *c, uint16_t addr);
-
-// Copies the accumulator value to the X register.
-void TAX(Control *c, uint16_t addr);
 
 // Stores de value of A to the current stack position.
 void PHA(Control *c, uint16_t addr);
@@ -25,9 +26,6 @@ void PHP(Control *c, uint16_t addr);
 // Pops the value from stack and assign it to A.
 void PLA(Control *c, uint16_t addr);
 
-//
-void TXS(Control *c, uint16_t addr);
-
 // Store value of A in memory.
 void STA(Control *c, uint16_t addr);
 
@@ -37,9 +35,8 @@ void STX(Control *c, uint16_t addr);
 // Store value of Y in memory.
 void STY(Control *c, uint16_t addr);
 
-// Triggers an interrupt request (IRQ).
-// TODO: Proper IRQ handling.
-void BRK(Control *c, uint16_t addr);
+// Copies the accumulator value to the X register.
+void TAX(Control *c, uint16_t addr);
 
 // Dummy instruction that does nothing.
 void ILL(Control *c, uint16_t addr);

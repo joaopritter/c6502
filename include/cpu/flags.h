5c04f2a7a817ef340e6a5dcd002560d464c7adf8
joaopritter <pedro.ritter@pm.me>
2026-03-26 00:40:50 -0300

    fix: missing flags

--- include/cpu/flags.h
+++ include/cpu/flags.h
@@ -4,13 +4,14 @@
 
 #include "cpu/control.h"
 
-#define NEGATIVE_FLAG           0b10000000
-#define OVERFLOW_FLAG           0b01000000
-#define B_FLAG                  0b00010000
-#define DECIMAL_FLAG            0b00001000
-#define INTERRUPT_DISABLE_FLAG  0b00000100
-#define ZERO_FLAG               0b00000010
-#define CARRY_FLAG              0b00000001
+#define NEGATIVE_FLAG 0b10000000
+#define OVERFLOW_FLAG 0b01000000
+#define EXTRA_FLAG 0b00100000
+#define BREAK_FLAG 0b00010000
+#define DECIMAL_FLAG 0b00001000
+#define INTERRUPT_DISABLE_FLAG 0b00000100
+#define ZERO_FLAG 0b00000010
+#define CARRY_FLAG 0b00000001
 
 int check_flag(Control *c, int flag);
 

dc3771648e7feae5d47d2b9cc1a3e12aa76ae9e2
joaopritter <pedro.ritter@pm.me>
2026-03-26 23:48:05 -0300

    fix: indirect address mode

--- src/addr_mode.c
+++ src/addr_mode.c
@@ -35,9 +35,15 @@ uint16_t ZRY(C6502 *c) {
 }
 
 uint16_t IND(C6502 *c) {
-  uint8_t lo = mem_read(c, c->pc++);
-  uint8_t hi = mem_read(c, c->pc++);
-  return (uint16_t)(lo + (hi << 8));
+  uint8_t ptr_lo = mem_read(c, c->pc++);
+  uint8_t ptr_hi = mem_read(c, c->pc++);
+  uint16_t ptr = (ptr_hi << 8) | ptr_lo;
+
+  uint8_t lo = mem_read(c, ptr);
+  uint16_t hi_addr = (ptr & 0xFF00) | ((ptr + 1) & 0x00FF); // 6502 hardware bug
+  uint8_t hi = mem_read(c, hi_addr);
+
+  return (hi << 8) | lo;
 }
 
 uint16_t IDX(C6502 *c) {
